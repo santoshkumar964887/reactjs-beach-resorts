6f5988f42b72fc153e47665cef372d48dc4ab049
Santosh Kumar <santoshkumar964887@gmail.com>
2021-01-15 18:54:33 +0530

    making file structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import React from "react";
+import "./App.css";
+import HomePage from "./pages/Home";
+import ErrorPage from "./pages/Error";
+import SingleRoomPage from "./pages/SingleRoom";
 function App() {
-  return <div className="App"> Hello santosh</div>;
+  return (
+    <>
+      <HomePage />
+      <SingleRoomPage />
+      <ErrorPage />
+    </>
+  );
 }
 
 export default App;
