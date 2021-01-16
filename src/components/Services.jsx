594547ff3319fcae41deb1c51f64f5c1a1e5f072
Santosh Kumar <santoshkumar964887@gmail.com>
2021-01-16 21:26:26 +0530

    making Services component

---
+++ src/components/Services.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import Title from "./Title";
+export default class services extends Component {
+  render() {
+    return (
+      <div>
+        <Title title="Services" />
+      </div>
+    );
+  }
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 import HeaderBackground from "../components/HeaderBackground";
 import Banner from "../components/Banner";
+import Services from "../components/Services";
 import { Link } from "react-router-dom";
 const Home = () => {
   return (
-    <HeaderBackground>
-      <Banner
-        title="Luxurious Rooms"
-        subtitle="Deluxe Rooms Starting At INR:5000"
-      >
-        <Link to="/rooms" className="btn-primary">
-          our rooms
-        </Link>
-      </Banner>
-    </HeaderBackground>
+    <>
+      <HeaderBackground>
+        <Banner
+          title="Luxurious Rooms"
+          subtitle="Deluxe Rooms Starting At INR:5000"
+        >
+          <Link to="/rooms" className="btn-primary">
+            our rooms
+          </Link>
+        </Banner>
+      </HeaderBackground>
+      <Services />
+    </>
   );
 };
 export default Home;
