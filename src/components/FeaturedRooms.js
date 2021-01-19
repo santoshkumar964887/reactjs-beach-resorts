c98c825a63aac06ff8b497176a2298d7984f910e
Santosh Kumar <santoshkumar964887@gmail.com>
2021-01-19 16:25:37 +0530

    completing loading component

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
 import { RoomContext } from "../context/RoomContext";
+import Loading from "./Loading";
 export class FeaturedRooms extends Component {
   static contextType = RoomContext;
   render() {
     const value = this.context;
-    return <div>{value} from feature</div>;
+    return (
+      <div>
+        {value} from feature
+        <Loading />
+      </div>
+    );
   }
 }
--- src/context/RoomContext.jsx
+++ src/context/RoomContext.jsx
@@ -8,6 +8,22 @@ class RoomProvider extends Component {
     featuredRooms: [],
     loading: true,
   };
+  //getData
+  componentDidMount() {
+    let rooms = this.FormateData(items);
+    let featuredRooms = rooms.filter((room) => room.featured === true);
+    this.setState({ rooms, sortedRooms: rooms, featuredRooms, loading: false });
+  }
+  FormateData(items) {
+    let tempItems = items.map((item) => {
+      let id = item.sys.id;
+      let images = item.fields.images.map((image) => image.fields.file.url);
+      let room = { ...item.fields, images, id };
+      return room;
+    });
+    return tempItems;
+  }
+
   render() {
     return (
       <RoomContext.Provider value="santosh not">
