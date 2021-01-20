a384cb42fd67dd9b970f54e5cbd6d1a4d3cdb141
Santosh Kumar <santoshkumar964887@gmail.com>
2021-01-20 17:11:10 +0530

    completing SingleRoom component

--- src/pages/SingleRoom.jsx
+++ src/pages/SingleRoom.jsx
@@ -42,14 +42,49 @@ export default class SingleRoom extends Component {
       size,
       type,
     } = room;
+    const [firstImage, ...restImage] = images;
     return (
-      <StyledHero img={images[0] || this.state.defaultBcg}>
-        <Banner title={`${name} room`}>
-          <Link to="/rooms" className="btn-primary">
-            back to rooms
-          </Link>
-        </Banner>
-      </StyledHero>
+      <>
+        <StyledHero img={firstImage || this.state.defaultBcg}>
+          <Banner title={`${name} room`}>
+            <Link to="/rooms" className="btn-primary">
+              back to rooms
+            </Link>
+          </Banner>
+        </StyledHero>
+        <section className="single-room">
+          <div className="single-room-images">
+            {restImage.map((image, index) => (
+              <img key={index} src={image} alt={name} />
+            ))}
+          </div>
+          <div className="single-room-info">
+            <article className="desc">
+              <h3>Details</h3>
+              <p>{description}</p>
+            </article>
+            <article className="info">
+              <h3>Info</h3>
+              <h6>Price : INR{price * 7}</h6>
+              <h6>size : {size} SQFT</h6>
+              <h6>
+                max capacity :{" "}
+                {capacity > 1 ? `${capacity} people` : `${capacity} person`}
+              </h6>
+              <h6>{pets ? "pets allowed" : "no pets allowed"}</h6>
+              <h6>{breakfast ? "Free Breakfast" : null}</h6>
+            </article>
+          </div>
+        </section>
+        <section className="room-extras">
+          <h6>extras</h6>
+          <ul className="extras">
+            {extras.map((item, index) => (
+              <li key={index}>-{item}</li>
+            ))}
+          </ul>
+        </section>
+      </>
     );
   }
 }
